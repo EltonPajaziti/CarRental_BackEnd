d8be506b06e6b268b3ab860172f71dd25daeba34
EltonPajaziti <pajazitielton002@gmail.com>
2025-08-08 14:38:53 +0200

    added route for /my-profile endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,15 @@
 const express= require("express");
 const login= require("../controller/login");
 const signup= require("../controller/signup");
+const checkAuth= require("../auth/checkauth");
+
+const myProfile= require("../controller/myProfile");
 const router=express.Router();
 
 
 router.post("/register", signup);
 router.post("/login",login);
+router.get("/my-profile",checkAuth, myProfile); // me JWT
+
 
 module.exports=router;
